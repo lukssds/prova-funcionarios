e8f033017e552e47adb0989b02de50896ec1a5e6
lukssds <lukssds@gmail.com>
2020-05-29 20:01:11 -0300

    Leitura dos dados

--- src/funcionario/funcionarios.java
+++ src/funcionario/funcionarios.java
@@ -1,10 +1,52 @@
 package funcionario;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class funcionarios {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
+		String nome,nome1,nome2;
+		char sn;
+		int hrstrabalhadas,hrstrabalhadas1,hrstrabalhadas2, opcao,totalhoras;
+		double vlrhora;
+		
+	
+			
+		
+	
+		do {
+	
+			System.out.print("Nome: ");
+			nome=sc.next();
+			System.out.print("Horas Trabalhadas: ");
+			hrstrabalhadas=sc.nextInt();
+			System.out.print("Valor por hora: ");
+			vlrhora=sc.nextDouble();
+			System.out.print("Digitar outro (S/N)? ");
+			sn=sc.next().charAt(0);
+			
+			if (hrstrabalhadas < 0) {
+				System.out.println("As Horas Trabalhadas não podem ser negativas, tente novamente:");
+				hrstrabalhadas=sc.nextInt();
+				
+			}
+			else if (vlrhora < 0 ) {
+				System.out.println("O valor por hora não pode ser negativo, tente novamente");
+				vlrhora=sc.nextDouble();
+				
+				
+			} 
 
+			
+		} while(sn== 's'|| sn=='S') ;
+		
+		
+		
+		
+		
 	}
 
 }
